a61209eadca5cf2bf0de76c91ba345eb3981480c
Gustav Pursche <pursche@posteo.de>
2017-11-04 22:40:58 +0100

    Specify browserlist array

--- wordpress/wp-content/themes/nick-jaussi/postcss.config.js
+++ wordpress/wp-content/themes/nick-jaussi/postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')(),
+        require('autoprefixer')({
+          browsers: [
+            'Firefox ESR',
+            'last 2 Chrome versions',
+            'last 2 Firefox versions',
+            'Safari >= 7',
+            'iOS 7',
+          ],
+        }),
         require('cssnano')({
             preset: 'default',
         }),
