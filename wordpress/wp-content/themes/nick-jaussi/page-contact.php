667a46ca35a148044ee89f81abe7af98192efed6
Gustav Pursche <pursche@posteo.de>
2017-11-05 19:54:28 +0100

    Fix contact template

--- wordpress/wp-content/themes/nick-jaussi/page-contact.php
+++ wordpress/wp-content/themes/nick-jaussi/page-contact.php
@@ -1,30 +1,28 @@
 <?php get_header(); ?>
 
-<div class="js-background-map background-map">
-  <main class="main">
-    <div class="text">
-      <?php
-        while ( have_posts() ) : the_post();
-      ?>
+<main class="js-background-map background-map">
+  <div class="text">
+    <?php
+      while ( have_posts() ) : the_post();
+    ?>
 
-        <h1 class="text__title">
-          <?php the_title(); ?>
-        </h1>
+      <h1 class="text__title">
+        <?php the_title(); ?>
+      </h1>
 
-        <div class="text__author-image">
-          <?php the_post_thumbnail('portrait'); ?>
-        </div>
+      <div class="text__author-image">
+        <?php the_post_thumbnail('portrait'); ?>
+      </div>
 
-        <div class="text__content">
-          <?php the_content(); ?>
-        </div>
+      <div class="text__content">
+        <?php the_content(); ?>
+      </div>
 
-      <?php
-        endwhile;
-        wp_reset_query();
-      ?>
-    </div>
-  </main>
-</div>
+    <?php
+      endwhile;
+      wp_reset_query();
+    ?>
+  </div>
+</main>
 
 <?php get_footer(); ?>
